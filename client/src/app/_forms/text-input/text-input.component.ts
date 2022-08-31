cfaf9ef31d119082430769c81d1539cc6be41fb2
aoconnell099 <aoconnell099@gmail.com>
2022-08-30 23:17:33 -0500

    Implemented reactive forms

---
+++ client/src/app/_forms/text-input/text-input.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.css']
+})
+export class TextInputComponent implements ControlValueAccessor {
+  @Input() label: string;
+  @Input() type = 'text';
+
+  constructor(@Self() public ngControl: NgControl) { // The self decorator ensures that angular will always inject what's here locally into this component instead of reusing another instance of this.
+    this.ngControl.valueAccessor = this; // This gives access to the control inside of this component when you use it inside the register form
+  } 
+
+  writeValue(obj: any): void {
+  }
+
+  registerOnChange(fn: any): void {
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -9,20 +11,55 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  registerForm: FormGroup;
+  maxDate: Date;
+  validationErrors: string[] = []; // Need to initialize because we check for the length in the component html
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService,
+     private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() -18);
+  }
+
+  initializeForm() {
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required,
+        Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
+    });
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+      this.registerForm.controls.confirmPassword.updateValueAndValidity();
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => { /* Get access to the control the you attach the validator to. 
+                                              We're attaching to the confirmPassword control, and comparing 
+                                              it to whatever is being passed to the matchTo. In this case we're 
+                                              passing in the password that we want to compare this value to. 
+                                              If these passwords match, then return null and validation passes. 
+                                              If not then we attach a validator error called isMatching to the 
+                                              control and this will fail the form validation */
+      return control?.value === control?.parent?.controls[matchTo].value 
+        ? null : {isMatching: true}
+    }
   }
 
   register() {
-    this.accountService.register(this.model).subscribe(response => {
-      console.log(response);
+    this.accountService.register(this.registerForm.value).subscribe(response => {
+      this.router.navigateByUrl('/members');
       this.cancel();
     }), error => {
-      console.log(error);
-      this.toastr.error(error.error);
+      this.validationErrors = error;
     }
 
   }
