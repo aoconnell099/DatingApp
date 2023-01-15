578e0508164a855cbda587821226292d8e9a2cba
aoconnell099 <aoconnell099@gmail.com>
2023-01-14 22:02:35 -0600

    Fixed logout button

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -57,6 +57,7 @@ export class NavComponent implements OnInit {
     this.breakpoint$.subscribe(() =>
       this.breakpointChanged()
     );
+    //this.logout();
   }
 
   private breakpointChanged() {
